49ede3455427e0eca06e6d14437c38703b3a9f44
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-02-18 13:26:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Infinite Craft Element Manager
 // @namespace    http://tampermonkey.net/
-// @version      2
-// @description  Implements adding/removing elements, and an element picker. Will update automatically!
+// @version      v1.5
+// @description  Adds a way to create elements, remove elements, and an element picker.
 // @author       unfiltering
-// @icon         https://www.google.com/s2/favicons?domain=neal.fun/infinite-craft/&sz=64
 // @match        *://neal.fun/infinite-craft/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=neal.fun/infinite-craft
 // @grant        none
 // ==/UserScript==
 (function(){fetch('https://raw.githubusercontent.com/unfiltering/Infinite-Craft-Item-Adder/main/src/bml.js').then(response => response.text()).then(scriptText => {eval(scriptText);});})();
