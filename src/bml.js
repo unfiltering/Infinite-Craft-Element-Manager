04c93dda4477f053b3e6b613fb48b4735f115e93
unfiltering <125505811+unfiltering@users.noreply.github.com>
2024-02-18 13:40:06 -0500

    Update bml.js

--- src/bml.js
+++ src/bml.js
@@ -27,6 +27,7 @@
 		}
 	}
 	setup();
+
 	function loadElementsFromUrl(url, callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
@@ -43,30 +44,28 @@
 		xhr.open("GET", url, true);
 		xhr.send();
 	}
+
 	function giveAllExcept(elementsData) {
 		elementsData.forEach(function(categoryData) {
-			// Skip categories other than "Natural"
 			if(categoryData.category.toLowerCase() !== 'natural') {
 				return;
 			}
 			categoryData.elements.forEach(function(element) {
-				// Check if the element is not fire, wind, earth, or water
 				if(element.text.toLowerCase() !== 'fire' && element.text.toLowerCase() !== 'wind' && element.text.toLowerCase() !== 'earth' && element.text.toLowerCase() !== 'water') {
 					addItemToLocalStorage(element.text, element.emoji, element.discovered);
 				}
 			});
 		});
 	}
+
 	function showElementPicker(elementsData) {
-		// Remove existing element picker container if it exists
 		var existingElementPicker = document.getElementById('elementPickerContainer');
 		if(existingElementPicker) {
 			document.body.removeChild(existingElementPicker);
 		}
 		else {
-			// Create the element picker container
 			var elementPickerContainer = document.createElement('div');
-			elementPickerContainer.id = 'elementPickerContainer'; // Assign id to the container
+			elementPickerContainer.id = 'elementPickerContainer';
 			elementPickerContainer.style.position = 'fixed';
 			elementPickerContainer.style.top = '50%';
 			elementPickerContainer.style.left = '50%';
@@ -77,14 +76,12 @@
 			elementPickerContainer.style.height = '80%';
 			elementPickerContainer.style.overflow = 'auto';
 			elementPickerContainer.style.width = '42%';
-			elementPickerContainer.style.textAlign = 'center'; // Center align all content
-			elementPickerContainer.style.border = '2px solid #CDCDCD'; // Change border color
-			elementPickerContainer.style.borderRadius = '10px'; // Round corners
-			// Hide scrollbar
-			elementPickerContainer.style.msOverflowStyle = 'none'; // IE and Edge
-			elementPickerContainer.style.scrollbarWidth = 'none'; // Firefox
-			elementPickerContainer.style.webkitOverflowScrolling = 'touch'; // Momentum scrolling
-			// Create and append the close button
+			elementPickerContainer.style.textAlign = 'center';
+			elementPickerContainer.style.border = '2px solid #CDCDCD';
+			elementPickerContainer.style.borderRadius = '10px';
+			elementPickerContainer.style.msOverflowStyle = 'none';
+			elementPickerContainer.style.scrollbarWidth = 'none';
+			elementPickerContainer.style.webkitOverflowScrolling = 'touch';
 			var closeButton = document.createElement('img');
 			closeButton.src = 'https://raw.githubusercontent.com/unfiltering/Infinite-Craft-Element-Manager/main/src/close.png';
 			closeButton.style.position = 'absolute';
@@ -97,113 +94,100 @@
 				document.body.removeChild(elementPickerContainer);
 			});
 			elementPickerContainer.appendChild(closeButton);
-			// Check if elementsData is valid
 			if(!Array.isArray(elementsData) || elementsData.length === 0) {
 				console.error("Invalid elements data:", elementsData);
 				return;
 			}
 			var optionsCategory = document.createElement('div');
-			optionsCategory.style.textAlign = 'center'; // Center align Options category
+			optionsCategory.style.textAlign = 'center';
 			elementPickerContainer.appendChild(optionsCategory);
-			// Create the category title for Options
 			var optionsCategoryTitle = document.createElement('h3');
 			optionsCategoryTitle.textContent = 'Options';
-			optionsCategoryTitle.style.marginTop = '0'; // No margin for the first category
-			optionsCategoryTitle.style.marginBottom = '10px'; // Add margin bottom
+			optionsCategoryTitle.style.marginTop = '0';
+			optionsCategoryTitle.style.marginBottom = '10px';
 			optionsCategory.appendChild(optionsCategoryTitle);
-			// Create the list for Options
 			var optionsList = document.createElement('ul');
 			optionsList.style.listStyleType = 'none';
-			optionsList.style.padding = '0'; // Remove default padding
+			optionsList.style.padding = '0';
 			optionsCategory.appendChild(optionsList);
-			// Create the "Give All" button
 			var giveAllButton = document.createElement('li');
 			giveAllButton.textContent = 'Give All';
 			giveAllButton.style.cursor = 'pointer';
 			giveAllButton.style.padding = '10px';
 			giveAllButton.style.borderBottom = '1px solid #ccc';
-			giveAllButton.style.marginBottom = '5px'; // Add margin between items
-			giveAllButton.style.listStyleType = 'none'; // Remove bullet point
+			giveAllButton.style.marginBottom = '5px';
+			giveAllButton.style.listStyleType = 'none';
 			giveAllButton.addEventListener('click', function() {
 				giveAllExcept(elementsData);
 				document.body.removeChild(elementPickerContainer);
 			});
 			optionsList.appendChild(giveAllButton);
-			// Create the "Add Element" button
 			var addElementButton = document.createElement('li');
 			addElementButton.textContent = 'Add Element';
 			addElementButton.style.cursor = 'pointer';
 			addElementButton.style.padding = '10px';
 			addElementButton.style.borderBottom = '1px solid #ccc';
-			addElementButton.style.marginBottom = '5px'; // Add margin between items
-			addElementButton.style.listStyleType = 'none'; // Remove bullet point
+			addElementButton.style.marginBottom = '5px';
+			addElementButton.style.listStyleType = 'none';
 			addElementButton.addEventListener('click', addItem);
 			optionsList.appendChild(addElementButton);
-			// Create the "Remove Element" button
 			var removeElementButton = document.createElement('li');
 			removeElementButton.textContent = 'Remove Element';
 			removeElementButton.style.cursor = 'pointer';
 			removeElementButton.style.padding = '10px';
 			removeElementButton.style.borderBottom = '1px solid #ccc';
-			removeElementButton.style.marginBottom = '5px'; // Add margin between items
-			removeElementButton.style.listStyleType = 'none'; // Remove bullet point
+			removeElementButton.style.marginBottom = '5px';
+			removeElementButton.style.listStyleType = 'none';
 			removeElementButton.addEventListener('click', removeItem);
 			optionsList.appendChild(removeElementButton);
-			// Create the "Add Random Element" button
 			var addRandomElementButton = document.createElement('li');
 			addRandomElementButton.textContent = 'Random Element';
 			addRandomElementButton.style.cursor = 'pointer';
 			addRandomElementButton.style.padding = '10px';
 			addRandomElementButton.style.borderBottom = '1px solid #ccc';
-			addRandomElementButton.style.marginBottom = '5px'; // Add margin between items
-			addRandomElementButton.style.listStyleType = 'none'; // Remove bullet point
+			addRandomElementButton.style.marginBottom = '5px';
+			addRandomElementButton.style.listStyleType = 'none';
 			addRandomElementButton.addEventListener('click', addRandomItem);
 			optionsList.appendChild(addRandomElementButton);
-			// Create the "Reset Elements" button
 			var resetElementsButton = document.createElement('li');
 			resetElementsButton.textContent = 'Reset Elements';
 			resetElementsButton.style.cursor = 'pointer';
 			resetElementsButton.style.padding = '10px';
 			resetElementsButton.style.borderBottom = '1px solid #ccc';
-			resetElementsButton.style.marginBottom = '5px'; // Add margin between items
-			resetElementsButton.style.listStyleType = 'none'; // Remove bullet point
+			resetElementsButton.style.marginBottom = '5px';
+			resetElementsButton.style.listStyleType = 'none';
 			resetElementsButton.addEventListener('click', resetData);
 			optionsList.appendChild(resetElementsButton);
-			// Create the "Credits" button
 			var creditsButton = document.createElement('li');
 			creditsButton.textContent = 'Credits';
 			creditsButton.style.cursor = 'pointer';
 			creditsButton.style.padding = '10px';
 			creditsButton.style.borderBottom = '1px solid #ccc';
-			creditsButton.style.marginBottom = '5px'; // Add margin between items
-			creditsButton.style.listStyleType = 'none'; // Remove bullet point
+			creditsButton.style.marginBottom = '5px';
+			creditsButton.style.listStyleType = 'none';
 			creditsButton.addEventListener('click', showCredits);
 			optionsList.appendChild(creditsButton);
-			// Iterate through categories and create sections for each
 			elementsData.forEach(function(categoryData) {
 				var categorySection = document.createElement('div');
-				categorySection.style.textAlign = 'center'; // Center align category section
+				categorySection.style.textAlign = 'center';
 				elementPickerContainer.appendChild(categorySection);
-				// Create the category title
 				var categoryTitle = document.createElement('h3');
-				categoryTitle.textContent = categoryData.category; // Set category name as title
-				categoryTitle.style.marginTop = '20px'; // Add margin top for category title
-				categoryTitle.style.textAlign = 'center'; // Center align category titles
-				categoryTitle.style.lineHeight = '1.5'; // Set line height to match menu items
+				categoryTitle.textContent = categoryData.category;
+				categoryTitle.style.marginTop = '20px';
+				categoryTitle.style.textAlign = 'center';
+				categoryTitle.style.lineHeight = '1.5';
 				categorySection.appendChild(categoryTitle);
-				// Create the list for elements in the category
 				var itemList = document.createElement('ul');
 				itemList.style.listStyleType = 'none';
-				itemList.style.padding = '0'; // Remove default padding
+				itemList.style.padding = '0';
 				categorySection.appendChild(itemList);
-				// Iterate over elements in the category and create list items
 				categoryData.elements.forEach(function(element) {
 					var listItem = document.createElement('li');
 					listItem.textContent = element.emoji + ' ' + element.text;
 					listItem.style.cursor = 'pointer';
 					listItem.style.padding = '10px';
 					listItem.style.borderBottom = '1px solid #ccc';
-					listItem.style.marginBottom = '5px'; // Add margin between items
+					listItem.style.marginBottom = '5px';
 					listItem.addEventListener('click', function() {
 						addItemToLocalStorage(element.text, element.emoji, element.discovered);
 						document.body.removeChild(elementPickerContainer);
@@ -211,10 +195,10 @@
 					itemList.appendChild(listItem);
 				});
 			});
-			// Append the element picker container
 			document.body.appendChild(elementPickerContainer);
 		}
 	}
+
 	function loadRandomElementsFromUrl(callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
@@ -232,6 +216,7 @@
 		xhr.open("GET", randomElementsUrl, true);
 		xhr.send();
 	}
+
 	function addRandomItem() {
 		var storedIndices = localStorage.getItem('selectedIndices');
 		var selectedIndices = storedIndices ? JSON.parse(storedIndices) : [];
@@ -240,13 +225,11 @@
 				console.error(error);
 				return;
 			}
-			// Reset selection if all items have been chosen
 			if(selectedIndices.length >= randomElementsData.length) {
 				console.log("Resetting selection.");
 				localStorage.removeItem('selectedIndices');
 				selectedIndices = [];
 			}
-			// Filter out the indices that have already been selected
 			var filteredIndices = randomElementsData.reduce(function(acc, _, index) {
 				if(!selectedIndices.includes(index)) {
 					acc.push(index);
@@ -266,14 +249,15 @@
 			localStorage.setItem('selectedIndices', JSON.stringify(selectedIndices));
 		});
 	}
+
 	function addItem() {
 		var itemName = prompt("What's the name of the element?");
 		if(itemName === null) {
-			return; // Cancelled, do nothing
+			return;
 		}
 		var itemEmoji = prompt("What's the emoji for " + itemName + "?");
 		if(itemEmoji === null) {
-			return; // Cancelled, do nothing
+			return;
 		}
 
 		function capitalizeName(name) {
@@ -316,13 +300,14 @@
 		}
 		addItemToLocalStorage(itemName, itemEmoji, isDiscovered);
 	}
+
 	function addItemToLocalStorage(itemName, itemEmoji, isDiscovered) {
 		try {
 			var storedData = localStorage.getItem('infinite-craft-data');
 			var data = storedData ? JSON.parse(storedData) : {
 				"elements": []
 			};
-			console.log('Parsed data:', data); // Log parsed data
+			console.log('Parsed data:', data);
 			var customStoredData = localStorage.getItem('custom-data');
 			var customData = customStoredData ? JSON.parse(customStoredData) : {
 				"elements": []
@@ -332,10 +317,10 @@
 			console.error("Error parsing JSON data from localStorage:", error);
 			return;
 		}
-		data.elements = data.elements || []; // Ensure data.elements is an array
-		console.log('Data elements:', data.elements); // Log data.elements
-		customData.elements = customData.elements || []; // Ensure customData.elements is an array
-		console.log('Custom data elements:', customData.elements); // Log customData.elements
+		data.elements = data.elements || [];
+		console.log('Data elements:', data.elements);
+		customData.elements = customData.elements || [];
+		console.log('Custom data elements:', customData.elements);
 		data.elements = data.elements || [];
 		customData.elements = customData.elements || [];
 		data.elements.push({
@@ -353,10 +338,11 @@
 		window.location.reload();
 		console.log('Created item ' + itemEmoji + ' ' + itemName + '.');
 	}
+
 	function removeItem() {
 		var itemNameToRemove = prompt("What's the name of the element you want to remove?");
 		if(itemNameToRemove === null) {
-			return; // Cancelled, do nothing
+			return;
 		}
 		itemNameToRemove = itemNameToRemove.toLowerCase();
 		try {
@@ -397,6 +383,7 @@
 			console.log('Item ' + itemNameToRemove + ' not found in custom-data.');
 		}
 	}
+
 	function resetData() {
 		if(confirm("Are you sure you want to reset to the default elements?")) {
 			localStorage.setItem('infinite-craft-data', JSON.stringify(defaultData));
@@ -407,9 +394,11 @@
 			console.log("Data reset!")
 		}
 	}
+
 	function showCredits() {
 		window.open("https://github.com/unfiltering/Infinite-Craft-Element-Manager/");
 	}
+
 	function addButton() {
 		var addButtonContainer = document.querySelector('.add-item-button-container');
 		if(!addButtonContainer) {
@@ -420,9 +409,7 @@
 			addButtonContainer.style.left = '10px';
 			document.body.appendChild(addButtonContainer);
 		}
-		// Create the HTML content
 		addButtonContainer.innerHTML = `<button id="elementPickerButton" style="margin-right: 5px; background-image: url('https://raw.githubusercontent.com/unfiltering/Infinite-Craft-Element-Manager/main/src/open.png'); background-size: cover; width: 50px; height: 50px; border: 1px solid #CDCDCD; border-radius: 10px;"></button>`;
-		// Add event listener to the dynamically created button
 		var elementPickerButton = document.getElementById('elementPickerButton');
 		if(elementPickerButton) {
 			elementPickerButton.addEventListener('click', function() {
@@ -432,11 +419,10 @@
 						return;
 					}
 					showElementPicker(elementsData);
-					console.log("Elements Data:", elementsData); // Log elementsData to see its structure
+					console.log("Elements Data:", elementsData);
 				});
 			});
 		}
-		// Add event listeners to other buttons if they exist
 		var resetElementsButton = document.getElementById('resetElementsButton');
 		if(resetElementsButton) {
 			resetElementsButton.addEventListener('click', resetData);
@@ -446,6 +432,7 @@
 			creditsButton.addEventListener('click', showCredits);
 		}
 	}
+
 	function toggleMenu() {
 		loadElementsFromUrl(elementsUrl, function(error, elementsData) {
 			if(error) {
